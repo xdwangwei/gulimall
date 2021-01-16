d1128d0455d64fa6d0bf046b432760680c6cfc34
vivi <i@weiwang.email>
2021-01-16 12:07:40 +0800

    添加gulimall-cart服务pom.xml

---
+++ gulimall-cart/src/main/java/com/vivi/gulimall/cart/GulimallCartApplication.java
@@ -0,0 +1,14 @@
+package com.vivi.gulimall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GulimallCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCartApplication.class, args);
+    }
+
+}
---
+++ gulimall-cart/src/test/java/com/vivi/gulimall/cart/GulimallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivi.gulimall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
